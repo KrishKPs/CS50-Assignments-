5bb1e5d6820a68847fff692f059e7a4e5bce3c69
Krish Patel <krishpatel@Krishs-MacBook-Pro.local>
2024-08-24 11:26:53 -0700

    Started week2

---
+++ Week-1/deepthought.py
@@ -0,0 +1,9 @@
+def main():
+    
+  guess = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+  if guess == "42" or guess=="fourty-two" or guess=="fourty two": 
+    print("Yes")
+  else:
+    print("No")
+main() 
