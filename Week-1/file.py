4691ec03200d1b8763daeee13ab515f0369b8bb4
Krish Patel <krishpatel@Krishs-MacBook-Pro.local>
2024-08-25 19:35:42 -0700

    Push

---
+++ Week-1/file.py
@@ -0,0 +1,5 @@
+# My Python version is 3.9.6 so Match is not available in my Local Maching 
+
+# We can do this assignment by Using Match Statement 
+
+# create cases for each file types and print accordingly 
